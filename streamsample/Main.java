9a4d9bc3109e8228a9461b72ae5631774e8b17b1
Joshua Ortega <joshua.ortega@gcash.com>
2024-11-25 10:05:29 +0800

    streamsample update

--- streamsample/Main.java
+++ streamsample/Main.java
@@ -2,9 +2,11 @@ package oop.pillars.streamsample;
 
 import java.util.*;
 import java.math.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.Comparator.comparing;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -18,7 +20,7 @@ public class Main {
 
         allocations.add(new Allocation("", new BigDecimal("0.234798")));
         allocations.add(new Allocation("", new BigDecimal("0.095678")));
-        allocations.add(new Allocation("crypto", new BigDecimal("0.007448")));
+        allocations.add(new Allocation("", new BigDecimal("0.007448")));
         allocations.add(new Allocation("", new BigDecimal("0.439865")));
         allocations.add(new Allocation("", new BigDecimal("0.181233")));
         allocations.add(new Allocation("", new BigDecimal("0.040978")));
@@ -26,9 +28,13 @@ public class Main {
         // Ascending Order
         List<Allocation> ascendingOrder = allocations.stream().sorted(comparing(v -> v.getPercentage())).toList();
 
-        // Descending Order
-        List<Allocation> descendingOrder = allocations.stream().sorted(comparing(v -> v.getPercentage(), Comparator.reverseOrder())).toList();
-
+        // Sorted by Third Decimal Place
+        List<Allocation> thirdDecimalPlace = allocations.stream()
+                .sorted(Comparator.comparing(v -> v.getPercentage()
+                        .movePointRight(3) // Shift the decimal point 3 places
+                        .remainder(BigDecimal.TEN) // Get the third decimal digit
+                        .intValue())) // Convert to int for comparison
+                .toList();
         // 1st Task
 
         for(Allocation element: ascendingOrder){
@@ -41,20 +47,20 @@ public class Main {
 
         // 2nd Task
 
-        List<Allocation> bigThree = descendingOrder.stream().limit(3).toList();
+        List<Allocation> bigThree = thirdDecimalPlace.stream().limit(3).toList();
 
         for(Allocation element: bigThree){
-            roundUpSize.add(new Allocation(element.getName(), element.getPercentage().setScale(3, RoundingMode.HALF_UP)));
+            roundUpSize.add(new Allocation(element.getName(), element.getPercentage().setScale(3, RoundingMode.UP)));
         }
 
         BigDecimal totalPercentage2Ta = roundUpSize.stream()
                 .map(Allocation::getPercentage) // Extract the percentage
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
 
-        List<Allocation> smallThree = ascendingOrder.stream().limit(3).toList();
+        List<Allocation> smallThree = thirdDecimalPlace.stream().skip(thirdDecimalPlace.size() - 3).collect(Collectors.toList());
 
         for(Allocation element: smallThree){
-            roundDownSize.add(new Allocation(element.getName(), element.getPercentage().setScale(3, RoundingMode.HALF_DOWN)));
+            roundDownSize.add(new Allocation(element.getName(), element.getPercentage().setScale(3, RoundingMode.DOWN)));
         }
 
         BigDecimal totalPercentage2Tb = roundDownSize.stream()
@@ -64,6 +70,8 @@ public class Main {
 
         System.out.println("Task 1: "+ twoDecimalPlace);
         System.out.println("Total = " + totalPercentage1T + "\n");
+
+        System.out.println("Arranged based on Third Decimal Number" + "\n" + thirdDecimalPlace + "\n");
 
         System.out.println("Task 2:");
         System.out.println(roundUpSize);
